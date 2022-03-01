b59ab280084783306f2954b4ebe384260ddea16e
Lotus 98T <rpitty@live.com>
2022-03-01 16:54:53 +1000

    Description paragraphs to the Home page.

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,12 @@ import "./Home.css";
 const Home = () => {
     return (
         <div className="Home">
-            <h2>HOME</h2>
+            <h2>Front-end React Developer</h2>
+            <div className="description-wrapper">
+                <p>I'm a front-end React developer; I have many years of experience with object-oriented programming, and I have subsequently focused on front-end React web development.</p>
+                <p>I possess many fundamental web development skills, including: HTML5, CSS, Sass, responsive web design, Javascript (ES6/ES7), ReactJS, Git+Github, and source code debugging.</p>
+                <p>I've become familiar with CSS responsive layouts and styling (especially with flexbox layouts), while I'm currently learning many new things with React: with which I am becoming far more comfortable, and learning to use its many powerful features to develop web applications.</p>
+            </div>
         </div>
     );
 }
